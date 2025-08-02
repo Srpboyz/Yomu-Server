d05aae873a2a577fb7a8d5332e2282c67a794b35
Srpboyz <srpboyz69@gmail.com>
2025-08-01 18:08:41 -0700

    Added more access routes for the webpage

--- yomuserver/routes/web/core.py
+++ yomuserver/routes/web/core.py
@@ -12,11 +12,30 @@ STATIC_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "static
 class WebPageHandler(RouteHandler):
     BASE_PATH = ""
 
-    @get("/")
-    def get_homepage(self, request: HttpRequest):
+    def send_html(self) -> HttpResponse:
         with open(os.path.join(STATIC_FOLDER, "index.html")) as f:
             html = f.read()
         return HttpResponse(body=html)
+
+    @get("/")
+    def get_homepage(self, request: HttpRequest):
+        return self.send_html()
+
+    @get("/sources")
+    def get_sourcelist_page(self, request: HttpRequest):
+        return self.send_html()
+
+    @get("/source/<source_id:int>")
+    def get_sources_page(self, request: HttpRequest):
+        return self.send_html()
+
+    @get("/manga/<manga_id:int>/")
+    def get_mangacard_page(self, request: HttpRequest):
+        return self.send_html()
+
+    @get("/reader/<chapter_id:int>/")
+    def get_reader_page(self, request: HttpRequest):
+        return self.send_html()
 
     @get("/<file>")
     def get_file(self, request: HttpRequest):
